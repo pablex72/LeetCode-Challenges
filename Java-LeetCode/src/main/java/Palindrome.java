29e518857692a8d8d06a67ee6efcae86566a50c7
pablex72 <tonyp2hoy@gmail.com>
2023-05-28 23:58:09 -0400

    solution initial palindrome java leetcode

---
+++ Java-LeetCode/src/main/java/Palindrome.java
@@ -0,0 +1,14 @@
+public class Palindrome {
+    public boolean isPalindrome(int x) {
+        if(x<0)                                   // if input is negative then it will not be palindrome , since -101 become 101-
+            return false;
+        int reversed = 0;
+        int copyOfx = x;
+        while(copyOfx!=0){
+            reversed = reversed*10+copyOfx%10;   // constructing reverse for postive integer
+            copyOfx=copyOfx/10;
+        }                                        // reverse integer will be constructed
+
+        return x==reversed;                      // compare reverse integer value with original value and return ans
+    }
+}
